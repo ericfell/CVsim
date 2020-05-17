a20a1db11e206aa4b54694a0d10233a7bb5d1322
djFellyfell <39314565+djFellyfell@users.noreply.github.com>
2020-05-17 16:24:54 -0400

    Add files via upload

--- test_publish.py
+++ test_publish.py
@@ -17,16 +17,16 @@ R = spc.R
 
 # Part A: how to simulate schemes
 
-# example 1 electron schemes
+#1 electron schemes
 setup = OneElectronCV(-0.4, 0.4, 0, 1, 1, 1, 1e-6, 1e-6, 5, 298)
-potential, current = setup.reversible()
+potential1, current1 = setup.reversible()
 potential2, current2 = setup.quasireversible(0.4, 1e-3)
 # example 2 electron scheme
 setup2 = TwoElectronCV(0.5, -0.5, 0.1, -0.05, 1, 1, 1, 1e-6, 1e-6, 1e-6, 5, 298)
 potential3, current3 = setup2.quasireversible(0.5, 0.5, 1e-3, 1e-4)
 
 plt.figure(1)
-plt.plot(potential, current, label='$E_{r}$')
+plt.plot(potential1, current1, label='$E_{r}$')
 plt.plot(potential2, current2, label='$E_{q}$')
 plt.plot(potential3, current3, label='$E_{q}E_{q}$')
 plt.xlabel('Potential (V)')
@@ -39,29 +39,34 @@ plt.show()
 
 #for simplicity an 'experiment' dataset is simulated and then fit. Ideally you
 #would input real data
+
 ################## 1) 'experiment' data ######################################
 test1 =  OneElectronCV(0.3, -0.3, 0, 1, 1, 1, 1.5e-6, 1.1e-6, 5, 298)
-potential4, current4 = test1.quasireversible(0.5, 1e-4) 
+potential, current = test1.quasireversible(0.5, 1e-4) 
 plt.figure(2)
-plt.plot(potential4, current4, label='experiment')
+plt.plot(potential, current, label='experiment')
+
 ################## 2) test data into real function############################
-##or turn into function of time???
-pot2, pot3 = np.split(potential4, 2)
-n_pot = pot2 - (abs(potential4[0]) + abs(potential4[-1]) + 0.001)#make function, this must be total range scanned, first pass
+pot2, pot3 = np.split(potential, 2)
+#calculate total range scanned, first pass
+n_pot = pot2 - (abs(potential[0]) + abs(potential[-1]) + 0.001)#assumes 1 mV step
 new_p = np.concatenate((pot2, n_pot))   
 plt.figure(3)
-plt.plot(new_p, current4, label='experiment')
-################## 3) fit curve to data of real function########################
+plt.plot(new_p, current, label='experiment')
+
+################## 3) fit curve to data of real function#######################
 def func(x, a, b, c):
-    Esrt = x[-1]
-    Erev = np.split(x, 2)[0][-1]
-    setup1 =  OneElectronCV(Esrt, Erev, 0, 1, 1, 1, a, b, 5, 298)
+    Estart = x[-1]
+    Ereverse = np.split(x, 2)[0][-1]
+    #here is where you choose what is known and unknown
+    setup1 =  OneElectronCV(Estart, Ereverse, 0, 1, 1, 1, a, b, 5, 298)
     potential_fit, current_fit = setup1.quasireversible(0.5, c) 
     return current_fit
 
-popt, pcov = curve_fit(func, potential4, current4, bounds=(0, [1e-5, 1e-5, 1e-3]))#p0=(1e-9, 2e-9, 0.09)
-fitted_current = func(potential4, *popt)
-perr = np.sqrt(np.diag(pcov)) # calculates one standard deviation of the parameters
+popt, pcov = curve_fit(func, potential, current, bounds=(0, [1e-5, 1e-5, 1e-3]))#p0=(1e-9, 2e-9, 0.09)
+fitted_current = func(potential, *popt)
+sigma = np.sqrt(np.diag(pcov)) # one standard deviation of the parameters
+print([f"{x:.2E} +/- {y:.0E}" for x,y in zip(popt, sigma)])
 
 plt.figure(3)
 plt.plot(new_p, fitted_current, label='Fit')
@@ -69,10 +74,11 @@ plt.legend()
 plt.show()
 ##############4) turn fit back into CV form
 plt.figure(2)
-plt.plot(potential4, fitted_current, '--', label='Fit')
+plt.plot(potential, fitted_current, '--', label='Fit')
 
 plt.figure(4)
-plt.plot(new_p, (current4 - fitted_current))# / current4)*100)
+plt.plot(new_p, (current - fitted_current))# / current4)*100)
+#[((x - y / x)*100) for x,y in zip(current, fitted_current)]
 plt.ylabel('% Error (exp - fit)')
 
 #popt gives fitted vals
