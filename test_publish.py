4627ca513f4d48b1ee0ccc4fae56ce642daaaaa9
djFellyfell <39314565+djFellyfell@users.noreply.github.com>
2020-05-20 16:11:05 -0400

    Add files via upload

--- test_publish.py
+++ test_publish.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  4 13:51:46 2020
-
-@author: erico
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,10 +8,10 @@ from scipy.optimize import curve_fit
 
 F = spc.physical_constants['Faraday constant'][0]
 R = spc.R
-#####################################################################
+
 # Part A: how to simulate schemes
 
-#1 electron schemes
+# example 1 electron schemes
 setup = OneElectronCV(-0.4, 0.4, 0, 1, 1, 1, 1e-6, 1e-6, 5, 298)
 potential1, current1 = setup.reversible()
 potential2, current2 = setup.quasireversible(0.4, 1e-3)
@@ -27,37 +21,37 @@ setup2 = TwoElectronCV(0.5, -0.5, 0.1, -0.05, 1, 1, 1, 1e-6, 1e-6, 1e-6, 5, 298)
 potential3, current3 = setup2.quasireversible(0.5, 0.5, 1e-3, 1e-4)
 
 plt.figure(1)
-plt.plot(potential1, current1, label='$E_{r}$')
-plt.plot(potential2, current2, label='$E_{q}$')
-plt.plot(potential3, current3, label='$E_{q}E_{q}$')
+plt.plot(potential1, [x*1000 for x in current1], label='$E_{r}$')
+plt.plot(potential2, [x*1000 for x in current2], label='$E_{q}$')
+plt.plot(potential3, [x*1000 for x in current3], label='$E_{q}E_{q}$')
 plt.xlabel('Potential (V)')
-plt.ylabel('Current (A)')
+plt.ylabel('Current (mA)')
 plt.tight_layout()
 plt.legend()
 plt.show()
 ##############################################################################
 ##############################################################################
-##############################################################################
 # Part B: how to fit experimental data
 
 #for simplicity an 'experiment' dataset is simulated and then fit. Ideally you
-#would input real data
+#would input real data to fit
 
-################## 1) 'experiment' data ######################################
+################## 1) 'experiment' data 
 test1 =  OneElectronCV(0.3, -0.3, 0, 1, 1, 1, 1.5e-6, 1.1e-6, 5, 298)
-potential, current = test1.quasireversible(0.5, 1e-4) 
+potential, current = test1.quasireversible(0.5, 5e-4) 
 plt.figure(2)
-plt.plot(potential, current, label='experiment')
+plt.plot(potential, [x*1000 for x in current], label='experiment')
 
-################## 2) turn test data into real function########################
-new_p = np.linspace(1, int(test1.N_max), int(test1.N_max))
+################## 2) turn test data into real function
+new_p = np.linspace(1, test1.N_max, test1.N_max)
 plt.figure(3)
-plt.plot(new_p, current, label='experiment')
+plt.plot(new_p, [x*1000 for x in current], label='experiment')
 
-################## 3) fit curve to data of real function#######################
+################## 3) fit curve to data of real function
 def func(x, a, b, c): #modify variables as needed
     E_start = x[-1]
-    E_reverse = np.split(x, 2)[0][-1]
+    E_reverse = np.round(np.split(x, 2)[0][-1], 4)
+    print(E_reverse)
     #here is where you choose what is known and unknown
     setup1 =  OneElectronCV(E_start, E_reverse, 0, 1, 1, 1, a, b, 5, 298)
     potential_fit, current_fit = setup1.quasireversible(0.5, c) 
@@ -66,27 +60,30 @@ def func(x, a, b, c): #modify variables as needed
 popt, pcov = curve_fit(func, potential, current, bounds=(0, [1e-5, 1e-5, 1e-3]))#p0=(1e-9, 2e-9, 0.09)
 fitted_current = func(potential, *popt)
 sigma = np.sqrt(np.diag(pcov)) # one standard deviation of the parameters
+# print fits and std dev. for a,b,c etc.
 print([f"{x:.2E} +/- {y:.0E}" for x,y in zip(popt, sigma)])
 
 plt.figure(3)
-plt.plot(new_p, fitted_current, '--', label='Fit')
+plt.plot(new_p, [x*1000 for x in fitted_current], '--', label='Fit')
 plt.xlabel('Timestep')
-plt.ylabel('Current (A)')
+plt.ylabel('Current (mA)')
 plt.tight_layout()
 plt.legend()
 plt.show()
 
-############## 4) turn fit back into CV form and overlay experiment data######
+############## 4) turn fit back into CV form and overlay experiment data
 plt.figure(2)
-plt.plot(potential, fitted_current, '--', label='Fit')
+plt.plot(potential, [x*1000 for x in fitted_current], '--', label='Fit')
 plt.xlabel('Potential (V)')
-plt.ylabel('Current (A)')
+plt.ylabel('Current (mA)')
 plt.tight_layout()
 plt.legend()
 plt.show()
 
 plt.figure(4)
-plt.semilogy(new_p, [(abs((x - y) / x)*100) for x,y in zip(current, fitted_current)])
+plt.plot(new_p, [((x - y)*1e6) for x,y in zip(current, fitted_current)], '--')
 plt.xlabel('Timestep')
-plt.ylabel('% Absolute Error')
+plt.ylabel("Residuals ($\mu$A) ")
+plt.tight_layout()
+plt.show()
 
