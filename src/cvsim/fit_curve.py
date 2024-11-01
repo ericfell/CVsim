eb48c08b1fb3af6ec06a15dae60523d4fc38c735
Eric Fell <efell@g.harvard.edu>
2024-11-01 16:13:37 -0400

    add tests for fit_curve, type hints

--- src/cvsim/fit_curve.py
+++ src/cvsim/fit_curve.py
@@ -34,6 +34,19 @@ class FitMechanism(ABC):
         self.temperature = temperature
         self.start_voltage = round(voltage_to_fit[0] * 1000)
 
+        if len(voltage_to_fit) != len(self.current_to_fit):
+            raise ValueError("'voltage_to_fit' and 'current_to_fit' must be equal length")
+
+        for key, value in {
+            'step_size': self.step_size,
+            'scan_rate': self.scan_r,
+            'disk_radius': self.disk_radius,
+            'c_bulk': self.c_bulk,
+            'temperature': self.temperature,
+        }.items():
+            if value <= 0.0:
+                raise ValueError(f"'{key}' must be > 0.0")
+
         if round(voltage_to_fit[10] * 1000) > self.start_voltage:  # scan starts towards more positive
             self.reverse_voltage = round(max(voltage_to_fit) * 1000)
         else:  # scan starts towards more negative
@@ -65,44 +78,48 @@ class FitE_rev(FitMechanism):
             current_to_fit: list[float] | np.ndarray,
             scan_r: float,
             c_bulk: float,
-            step_size: float = 1.0,
-            disk_radius: float = 1.5,
-            temperature: float = 298.0,
+            step_size: float,
+            disk_radius: float,
+            temperature: float,
             reduction_potential: float | None = None,
             diffusion_reactant: float | None = None,
             diffusion_product: float | None = None
     ) -> None:
         super().__init__(voltage_to_fit, current_to_fit, scan_r, c_bulk, step_size, disk_radius, temperature)
-        self.reduction_potential = reduction_potential
-        self.diffusion_reactant = diffusion_reactant
+        self.reduction_potential = reduction_potential  # TODO move into abstract class?
+        self.diffusion_reactant = diffusion_reactant  # TODO move into abstract class?
         self.diffusion_product = diffusion_product
 
-        # print({k: v for k, v in locals().items() if k != 'self'})
         # Contains only variables with a user-specified fixed value
         self.fixed_vars = {
             'reduction_potential': reduction_potential,
             'diffusion_reactant': diffusion_reactant,
             'diffusion_product': diffusion_product,
         }
+        for key, value in self.fixed_vars.items():
+            if key in ['diffusion_reactant', 'diffusion_product'] and value is not None and value <= 0.0:
+                raise ValueError(f"'{key}' must be > 0.0 or None")
+
         self.fixed_vars = {k: v for k, v in self.fixed_vars.items() if v is not None}
 
         # Values are [initial guess, lower bound, upper bound]
         self.default_vars = {
-            'reduction_potential': [round((self.voltage_to_fit[np.argmax(self.current_to_fit)]
-                                           + self.voltage_to_fit[np.argmin(self.current_to_fit)]) / 2, 3),
-                                    round(min(self.start_voltage, self.reverse_voltage) / 1000, 3),
-                                    round(max(self.start_voltage, self.reverse_voltage) / 1000, 3),
-                                    ],
+            'reduction_potential': [
+                round((self.voltage_to_fit[np.argmax(self.current_to_fit)]
+                       + self.voltage_to_fit[np.argmin(self.current_to_fit)]) / 2, 3),
+                round(min(self.start_voltage, self.reverse_voltage) / 1000, 3),
+                round(max(self.start_voltage, self.reverse_voltage) / 1000, 3),
+            ],
             'diffusion_reactant': [1e-6, 5e-8, 1e-4],
             'diffusion_product': [1e-6, 5e-8, 1e-4],
         }
         # TODO incorrect inputs, error handling
 
     def fit(
-        self,
-        reduction_potential: None | float | tuple[float, float] | tuple[float, float, float] = None,
-        diffusion_reactant: None | float | tuple[float, float] | tuple[float, float, float] = None,
-        diffusion_product: None | float | tuple[float, float] | tuple[float, float, float] = None,
+            self,
+            reduction_potential: None | float | tuple[float, float] | tuple[float, float, float] = None,
+            diffusion_reactant: None | float | tuple[float, float] | tuple[float, float, float] = None,
+            diffusion_product: None | float | tuple[float, float] | tuple[float, float, float] = None,
     ) -> tuple[np.ndarray, np.ndarray]:
         """TODO  no guess | initial guess | bounds | guess, bounds"""
 
@@ -112,10 +129,6 @@ class FitE_rev(FitMechanism):
             'diffusion_product': diffusion_product,
         }
         fit_vars = {k: v for k, v in fit_vars.items() if v is not None}
-
-        for key, val in fit_vars.items():
-            if isinstance(val, tuple) and len(val) not in [2, 3]:
-                raise ValueError(f"'{key}' allowed types: None, float, tuple[float, float], tuple[float, float, float]")
 
         # check intersection of fixed_vars / fit_vars dicts. if so raise error
         intersection_errors = self.fixed_vars.keys() & fit_vars.keys()
@@ -146,13 +159,13 @@ class FitE_rev(FitMechanism):
             elif isinstance(value, tuple) and len(value) == 3:
                 if value[1] >= value[2]:
                     raise ValueError("Lower bound must be lower than upper bound")
-                fit_default_vars[param] = value
+                fit_default_vars[param] = list(value)
 
         for param, (initial, lower, upper) in fit_default_vars.items():
             if not lower < initial < upper:
-                # if default initial guess is outside bounds, set guess to avg of bounds
-                fit_default_vars[param] = ((lower + upper) / 2, lower, upper)
-                # if user's guess was outside bounds
+                # check if default initial guess is outside bounds, set guess to avg of bounds
+                fit_default_vars[param] = [(lower + upper) / 2, lower, upper]
+                # check if user's guess was outside bounds
                 if initial != self.default_vars[param][0]:
                     raise ValueError(f"Initial guess for '{param}' is outside user-defined bounds")
 
@@ -186,18 +199,26 @@ class FitE_rev(FitMechanism):
                 temperature=self.temperature,
             ).simulate()
             return i_fit
-        print(f"data len: x {len(self.voltage_to_fit)} y {len(self.current_to_fit)}")
-        popt, pcov = curve_fit(f=fit_function,
-                               xdata=self.voltage_to_fit,
-                               ydata=np.insert(self.current_to_fit, 0, 0),#[1:],
-                               p0=initial_guesses,
-                               bounds=[lower_bounds, upper_bounds])
 
+        # fit raw data but exclude first data point, as semi-analytical method skips time=0
+        fit_results = curve_fit(
+            f=fit_function,
+            xdata=self.voltage_to_fit,
+            ydata=self.current_to_fit[1:],
+            p0=initial_guesses,
+            bounds=[lower_bounds, upper_bounds],
+        )
+        # TODO: return the optimal parameters, transform popt from an array into a dict keyed by fitting param name?
+        popt, pcov = list(fit_results)
         current_fit = fit_function(self.voltage_to_fit, *popt)
         sigma = np.sqrt(np.diag(pcov))  # one standard deviation of the parameters
 
         for val, error, param in zip(popt, sigma, fitting_params):
             print(f"Final fit: '{param}': {val:.2E} +/- {error:.0E}")
-        print(f"Ill-conditioned if large: {np.linalg.cond(pcov)}")
-        # TODO: return the optimal parameters, transform popt from an array into a dict keyed by fitting param name?
+        print(f"Ill-conditioned if large: {np.linalg.cond(pcov)}")  # remove
+
+        # Semi-analytical method does not compute the first point (i.e. time=0)
+        # so the starting voltage data point with a zero current is reinserted
+        self.voltage_to_fit = np.insert(self.voltage_to_fit, 0, self.start_voltage / 1000)
+        current_fit = np.insert(current_fit, 0, 0)
         return self.voltage_to_fit, current_fit
